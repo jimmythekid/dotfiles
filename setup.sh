55ad3a2b6f976a4cd2586fb59bd3775ce6d65132
jimmythekid <jimmy.mcerlain@gmail.com>
2014-12-11 21:58:47 +0000

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "making .dotfile directory"
+mkdir .dotfiles
+cd .dotfiles
+
+echo "cloning in the remote repo"
+git clone git@github.com:jimmythekid/dotfiles.git .dotfiles
+
+echo "removing old files and adding sim links to new"
+rm -r .vim .vimrc .screenrc .gitconfig
+ln -s .dotfiles/vim .vim
+ln -s .dotfiles/vimrc .vimrc
+ln -s .dotfiles/screenrc .screenrc
+ln -s .dotfiles/gitconfig .gitconfig
