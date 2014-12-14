07ba2c84aa3a3739d3640e919785634de21bddad
jimmythekid <jimmy.mcerlain@gmail.com>
2014-12-14 14:43:46 -0800

    added iTerm Colors

--- setup.sh
+++ setup.sh
@@ -11,16 +11,19 @@ sudo apt-get install openjdk-7-jdk
 
 echo "making .dotfile directory"
 mkdir .dotfiles
-cd .dotfiles
+#cd .dotfiles
 
 echo "cloning in the remote repo"
-git clone git@github.com:jimmythekid/dotfiles.git .dotfiles
+git clone git@github.com:jimmythekid/dotfiles.git ~/.dotfiles
+cd .dotfiles
+rm -R .dotfiles/vim/bundle
+
 
 echo "pulling in NeoBundle packages"
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
 echo "removing old files and adding sim links to new"
-rm -r .vim .vimrc .screenrc .gitconfig
+sudo rm -r .vim .vimrc .screenrc .gitconfig
 ln -s .dotfiles/vim .vim
 ln -s .dotfiles/vimrc .vimrc
 ln -s .dotfiles/screenrc .screenrc
